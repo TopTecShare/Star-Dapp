3ecbe2dbe0f42089a116e5aed430647911f362e5
TopTecShare <eestimorol.ice9208g@gmail.com>
2018-10-20 11:25:23 +0200

    changed truffle.js as a windows env it required to have truffle-config.js

--- truffle.js
+++ truffle.js
@@ -13,6 +13,17 @@
  */
 
 module.exports = {
+
+
+    networks:{
+      development:{
+      host:"127.0.0.1",
+      port:"7545",
+      gas:4600000,
+      network_id:"*"
+      }
+
+              }
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
 };
